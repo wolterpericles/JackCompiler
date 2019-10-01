399efae01ee14b7cfc6cee9f2c6c86d94851e481
wolterpericles <wpericles1@gmail.com>
2019-10-01 08:34:36 -0300

    JackCompiler

--- src/com/wolter/jackcompiler/jackcompiler/Main.java
+++ src/com/wolter/jackcompiler/jackcompiler/Main.java
@@ -19,8 +19,9 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        /*
         JackTokenizer jack;
-        jack = new JackTokenizer("./Exemplos_Jack/Main.jack");
+        jack = new JackTokenizer("./Exemplos_Jack/Square.jack");
         System.out.println("<tokens>");
         while(jack.hasMoreTokens())
         {
@@ -28,6 +29,7 @@ public class Main {
             System.out.println(jack.tagToken());
         }
         System.out.println("</tokens>");
+        */
     }
     
 }
